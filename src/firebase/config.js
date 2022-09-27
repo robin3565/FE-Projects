f3b30c2ff1d2fc15deaf3d0da6523f8da84e8e33
Robin Kim <107474891+robin3565@users.noreply.github.com>
2022-09-27 16:04:51 +0900

    api key

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,13 +4,12 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAHbPPPmLnbk6aV_mfO-OwCb-ZOHaQvh0M",
-  authDomain: "instagram-clone-6ebc5.firebaseapp.com",
-  projectId: "instagram-clone-6ebc5",
-  storageBucket: "instagram-clone-6ebc5.appspot.com",
-  messagingSenderId: "182476848504",
-  appId: "1:182476848504:web:93a471fe5ad15392fd9c8d",
-  storageBucket: "instagram-clone-6ebc5.appspot.com"
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
